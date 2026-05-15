411c395a94fbcabd0e7f421909ba770a19901516
vietbui99 <buiquocviet99@gmail.com>
2026-05-15 19:16:43 -0400

    fix slug
    Signed-off-by: vietbui99 <buiquocviet99@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toSlug(s: string): string {
+  return s.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-|-$/g, "");
+}
